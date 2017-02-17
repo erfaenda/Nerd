f2b41e820d8d4a5349e4f3496fb6b4ce2d5c6b93
alex <regalko7@gmail.com>
2017-02-17 08:55:56 +0500

    ex

--- src/DopClass12.java
+++ src/DopClass12.java
@@ -2,7 +2,7 @@
  * Created by alex on 17.02.17.
  */
 public class DopClass12 {
-    public void KiraSay(){
-        System.out.println("Это класс Киры он дополнительный");
+    public void KiraSay(String name){
+        System.out.println(name + " - крутой чувачечек");
     }
 }
--- src/ManyClass12.java
+++ src/ManyClass12.java
@@ -1,10 +1,16 @@
 /**
  * Created by alex on 17.02.17.
  */
+import java.util.Scanner;
+
 public class ManyClass12 {
     public static void main(String[] args) {
+        Scanner enter = new Scanner(System.in);
         DopClass12 Kira = new DopClass12();
-        Kira.KiraSay();
+        System.out.println("Скажи имя:");
+
+        String name = enter.nextLine();
+        Kira.KiraSay(name);
 
     }
 }
