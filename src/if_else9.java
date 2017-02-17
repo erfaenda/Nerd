fb2eb1ff7590242a24134502d3b8f3ec52d07d41
alex <regalko7@gmail.com>
2017-02-17 09:15:52 +0500

    Kira & L

---
+++ src/if_else9.java
@@ -0,0 +1,25 @@
+/**
+ * Created by alex on 17.02.17.
+ */
+import java.util.Scanner;
+
+public class if_else9 {
+    public static void main(String[] args) {
+        System.out.println("Сколько лет Мише?");
+        Scanner enterM = new Scanner(System.in);
+        int Misha = enterM.nextInt();
+
+        System.out.println("Сколько лет Леше?");
+        Scanner enterA = new Scanner(System.in);
+        int Alesha = enterA.nextInt();
+
+
+
+        if (Misha > Alesha) {
+            System.out.println("Мише " + Misha + " лет, он старше Алеши");
+        }
+        else {
+            System.out.println("Алеша старше Миши, ему " + Alesha + " лет");
+        }
+    }
+}
