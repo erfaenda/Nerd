2ca4c9b2da44c632ce4d5dd089d21c78c2d8c927
alex <regalko7@gmail.com>
2017-02-17 08:51:04 +0500

    ex

---
+++ src/DopClass12.java
@@ -0,0 +1,8 @@
+/**
+ * Created by alex on 17.02.17.
+ */
+public class DopClass12 {
+    public void KiraSay(){
+        System.out.println("Это класс Киры он дополнительный");
+    }
+}
---
+++ src/ManyClass12.java
@@ -0,0 +1,10 @@
+/**
+ * Created by alex on 17.02.17.
+ */
+public class ManyClass12 {
+    public static void main(String[] args) {
+        DopClass12 Kira = new DopClass12();
+        Kira.KiraSay();
+
+    }
+}
