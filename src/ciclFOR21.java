2814b79d9ac822c9f329dc6d1ff1a05f5bbf1081
alex <regalko7@gmail.com>
2017-02-20 17:15:17 +0500

    Kira & L

---
+++ src/ciclFOR21.java
@@ -0,0 +1,10 @@
+/**
+ * Created by alex on 20.02.17.
+ */
+public class ciclFOR21 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i+=2){
+            System.out.println(i);
+        }
+    }
+}
