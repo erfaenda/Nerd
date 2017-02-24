0838c760aa232b2d1ec17babb34ff4cf8226b174
erfaenda <regalko7@gmail.com>
2017-02-24 21:35:57 +0500

    next

--- src/BankProcent22.java
+++ src/BankProcent22.java
@@ -9,7 +9,7 @@ public class BankProcent22 {
 
         for (int year = 1; year <= 30; year++) {
             vklad += vklad*procent;
-            System.out.println("За " + year + " год, на ваша переплата по счету составит " + vklad + " сумма рублей!" );
+            System.out.println("За " + year + " год, на ваша переплата по счету составит " + (vklad - 816000) + " рублей!" );
         }
         itog = vklad - 816000;
         System.out.println("Общая переплата по кредиту равна: " + itog + " рублей");
