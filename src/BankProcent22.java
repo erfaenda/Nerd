c6021d06a69ede9e1079b90c28667f8bcda36aaf
erfaenda <regalko7@gmail.com>
2017-02-24 21:32:43 +0500

    next

--- src/BankProcent22.java
+++ src/BankProcent22.java
@@ -5,10 +5,13 @@ public class BankProcent22 {
     public static void main(String[] args) {
         double vklad = 816000;
         double procent = 0.05;
+        double itog = 0;
 
         for (int year = 1; year <= 30; year++) {
             vklad += vklad*procent;
-            System.out.println("За " + year + " год, на Вашем счету появилось " + vklad + " сумма долларов!" );
+            System.out.println("За " + year + " год, на ваша переплата по счету составит " + vklad + " сумма рублей!" );
         }
+        itog = vklad - 816000;
+        System.out.println("Общая переплата по кредиту равна: " + itog + " рублей");
     }
 }
---
+++ src/voennoeVremya35.java
@@ -0,0 +1,13 @@
+/**
+ * Created by alex on 24.02.2017.
+ */
+public class voennoeVremya35 {
+    public static void main(String[] args) {
+        voennoe35 time = new voennoe35();
+        System.out.println(time.toMilitary());
+
+        time.setTime(15,55,68);
+
+        System.out.println(time.toMilitary());
+    }
+}
