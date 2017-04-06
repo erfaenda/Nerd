d1f48684dc24d28b7586d6482e4fa48c861bc406
alex <regalko7@gmail.com>
2017-04-06 14:30:41 +0500

    kjkrf

--- src/GUI/GUI_2/Main.java
+++ src/GUI/GUI_2/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Alex wnd = new Alex();//создали обьект класса с конструктором
         wnd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//присвоили значение по умолчанию для кнопки с крестиком
-        wnd.setSize(400, 200);//установили размер окна
+        wnd.setSize(400, 200);//установили размер окна!
         wnd.setVisible(true);//установили видимость окна
     }
 }
